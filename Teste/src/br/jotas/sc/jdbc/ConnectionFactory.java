b3f205fb0f6db23d1186df976703cd2695949b9d
Jair Perrut <jairperrut@gmail.com>
2014-11-10 21:05:42 -0200

    Ajuste na classe de conexão

--- Teste/src/br/jotas/sc/jdbc/ConnectionFactory.java
+++ Teste/src/br/jotas/sc/jdbc/ConnectionFactory.java
@@ -4,16 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.management.RuntimeErrorException;
-
 public class ConnectionFactory {
-	
-	public Connection getConnection(){
-		try{
-			return DriverManager.getConnection("jdbc:mysql://localhost/jotaslocadoraDB","root","123123");				
-		}catch(SQLException e){
-			throw new RuntimeException(e);
+	private static Connection con;
+
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/jotaslocadoraDB", "root", "");
+			con.setAutoCommit(false);
+			return con;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		return null;
 	}
 
+	 public static void closeConnection(){
+	        try {
+	            con.close();
+	        } catch (SQLException ex) {
+	            ex.printStackTrace();
+	        }
+	    }
 }
